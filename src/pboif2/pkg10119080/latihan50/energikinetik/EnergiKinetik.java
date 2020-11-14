f9a5b620ded25267ea6529f3038504a22acc7095
PratamaVin7 <kevinpratamaoke@gmail.com>
2020-11-14 12:15:43 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan50/energikinetik/EnergiKinetik.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan50.energikinetik;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * (OBJECT ORIENTED) Energi Kinetik
+ */
+class EnergiKinetik {
+    private double massa, kecepatan;
+    
+    public double getMassa() {
+        return this.massa / 1000;
+    }
+    
+    public void setMassa(double massa) {
+        this.massa = massa;
+    }
+    
+    public double getKecepatan() {
+        return this.kecepatan;
+    }
+    
+    public void setKecepatan(double kecepatan) {
+        this.kecepatan = kecepatan;
+    }
+    
+    public double hitungEnergiKinetik(double massa, double kecepatan) {
+        return 0.5 * massa * Math.pow(kecepatan, 2);
+    }
+    
+    public double hitungUsaha(double kecepatanAwal) {
+        return 0.5 * massa * (Math.pow(kecepatan, 2) - Math.pow(kecepatanAwal, 2));
+    }
+    
+}
